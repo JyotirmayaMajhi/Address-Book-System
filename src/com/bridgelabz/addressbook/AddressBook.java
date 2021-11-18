28ba8805ecad89fa871ecdd7bee56c274ad56ff9
JyotirmayaMajhi <jyotirmayamajhi1998@gamil.com>
2021-11-18 17:49:37 +0530

    Added New Contact Address

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
 public class AddressBook {
 	private final String firstName;
 	private final String lastName;
@@ -22,8 +23,79 @@ public class AddressBook {
 		this.phoneNumber = phoneNumber;
 		this.emailID = emailID;
 	}
+	
+	public AddressBook(String firstName2, String lastName2, String address2, String cityName2, String stateName2,
+			String zip2, String phoneNumber2) {
+				this.firstName = "";
+				this.lastName = "";
+				this.address = "";
+				this.cityName = "";
+				this.stateName = "";
+				this.zip = 0;
+				this.phoneNumber = 0;
+				this.emailID = "";
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", cityName="
+				+ cityName + ", stateName=" + stateName + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
+	public static AddressBook contactPerson() {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("first name : ");
+		String firstName = scanner.next();
+		System.out.println("last name : ");
+		String lastName = scanner.next();
+		System.out.println("address : ");
+		String address = scanner.next();
+		System.out.println("city name : ");
+		String cityName = scanner.next();
+		System.out.println("state name : ");
+		String stateName = scanner.next();
+		System.out.println("zip : ");
+		String zip = scanner.next();
+		System.out.println("phone number : ");
+		String phoneNumber = scanner.next();
+		
+		AddressBook addressBook = new AddressBook(firstName, lastName, address, cityName, stateName, zip,
+				phoneNumber);
+		return addressBook;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to the address book program");
+		AddressBook addressBook =contactPerson();
+		System.out.println(addressBook);
 	}
 }
